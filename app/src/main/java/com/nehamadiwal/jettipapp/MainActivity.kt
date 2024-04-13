f3c8f1637632793b46deb55e1ecbf1d25ba6aa88
Neha Madiwal <nehamadiwal@gmail.com>
2024-04-13 18:15:52 +0530

    integrate custom edit text with MainActivity

--- app/src/main/java/com/nehamadiwal/jettipapp/MainActivity.kt
+++ app/src/main/java/com/nehamadiwal/jettipapp/MainActivity.kt
@@ -12,17 +12,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nehamadiwal.jettipapp.components.InputField
 import com.nehamadiwal.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApp {
                 TopHeader()
+                MainContent()
             }
         }
     }
@@ -84,6 +90,15 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
 @Preview
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyBoardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -91,11 +106,18 @@ fun MainContent() {
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-        Column() {
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
+        Column {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    // TODO: onValueChanged
+                    keyBoardController?.hide()
+                }
+            )
         }
     }
 }
